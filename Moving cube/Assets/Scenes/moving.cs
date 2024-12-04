bf55408b58886d405f2aba2311a281ac0447a27c
bernard_BDLT <bernatonkawaii@gmail.com>
2024-12-04 22:23:05 +0100

    beug de direction reglé, ajouter collisions et réduire mouvement de tete en haut et en bas a un degre moins grand

--- Moving cube/Assets/Scenes/moving.cs
+++ Moving cube/Assets/Scenes/moving.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class moving : MonoBehaviour
 {
-    public float speed = .05f;// init la vitesse du cube
-    public float sensitivityX = 10f;  // Sensibilité pour la rotation autour de l'axe X (horizontal)
-    public float sensitivityY = 10f;  // Sensibilité pour la rotation autour de l'axe Y (vertical)
+    public float speed = .05f; // init la vitesse du cube
+    public float sensitivityX = 0.1f;  // Sensibilité pour la rotation autour de l'axe X (horizontal)
+    public float sensitivityY = 0.1f;  // Sensibilité pour la rotation autour de l'axe Y (vertical)
 
     private float rotationX = 0f;     // Stocke la rotation autour de l'axe X
     private float rotationY = 0f;     // Stocke la rotation autour de l'axe Y
@@ -14,12 +14,12 @@ public class moving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Déplacement en fonction de la direction où le cube fait face
         float xDir = Input.GetAxis("Horizontal"); 
         float yDir = Input.GetAxis("Vertical");
 
-        Vector3 moving = new Vector3(xDir, 0.0f, yDir);
+        Vector3 moving = transform.forward * yDir + transform.right * xDir;
         transform.position += moving * speed;
-
 
         // Récupère le mouvement de la souris
         float mouseX = Input.GetAxis("Mouse X");
