6810c54990fcc85651468e83a16ca55becbe84f2
bernard_BDLT <bernatonkawaii@gmail.com>
2025-01-16 19:29:02 +0100

    j'ai merdé ça c'est le nouveau projet sur lequel tu dois push coco

---
+++ Jaimerdémybad/Assets/Scenes/Ennemy_behavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ennemy_behavior : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ///faire en sorte que les ennemis spawn.
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
